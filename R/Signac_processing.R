3f1b19624801b66d6ebfc203e4e5222604d8dab3
qingnanl <53788946+qingnanl@users.noreply.github.com>
2022-01-17 23:20:06 -0600

    Create Signac_processing.R

---
+++ R/Signac_processing.R
@@ -0,0 +1,112 @@
+# We want to use a preprocessed 10x multiome data and combine that with our ALTSS output.
+# Thus, here I show a routine processing of single-cell multiome (10x) data using Signac. Basically I want to show how the final 'pbmc.rds' is generated.
+# This is pretty much the same thing as the offcial Signac tutorial: https://satijalab.org/signac/articles/pbmc_multiomic.html 
+
+# read libraries
+library(Signac)
+library(Seurat)
+library(EnsDb.Hsapiens.v86)
+library(BSgenome.Hsapiens.UCSC.hg38)
+
+# load the RNA and ATAC data
+# the data is downloaded from https://www.10xgenomics.com/cn/resources/datasets (go to Single Cell Multiome ATAC + Gene Expression; pbmc_granulocyte_sorted_3k)
+#
+counts <- Read10X_h5("~/pbmc_granulocyte_sorted_3k_filtered_feature_bc_matrix.h5")
+fragpath <- "~/pbmc_granulocyte_sorted_3k_atac_fragments.tsv.gz"
+
+# get gene annotations for hg38
+annotation <- GetGRangesFromEnsDb(ensdb = EnsDb.Hsapiens.v86)
+seqlevelsStyle(annotation) <- "UCSC"
+genome(annotation) <- "hg38"
+
+# create a Seurat object containing the RNA adata
+pbmc <- CreateSeuratObject(
+  counts = counts$`Gene Expression`,
+  assay = "RNA"
+)
+
+# create ATAC assay and add it to the object
+pbmc[["ATAC"]] <- CreateChromatinAssay(
+  counts = counts$Peaks,
+  sep = c(":", "-"),
+  fragments = fragpath,
+  annotation = annotation
+)
+# pbmc
+DefaultAssay(pbmc) <- "ATAC"
+
+pbmc <- NucleosomeSignal(pbmc)
+pbmc <- TSSEnrichment(pbmc)
+
+# filter out low quality cells
+pbmc <- subset(
+  x = pbmc,
+  subset = nCount_ATAC < 100000 &
+    nCount_RNA < 25000 &
+    nCount_ATAC > 1000 &
+    nCount_RNA > 1000 &
+    nucleosome_signal < 2 &
+    TSS.enrichment > 1
+)
+pbmc
+# call peaks using MACS2
+peaks <- CallPeaks(pbmc, macs2.path = "~/bin/macs2")
+
+# remove peaks on nonstandard chromosomes and in genomic blacklist regions
+peaks <- keepStandardChromosomes(peaks, pruning.mode = "coarse")
+peaks <- subsetByOverlaps(x = peaks, ranges = blacklist_hg38_unified, invert = TRUE)
+
+# quantify counts in each peak
+macs2_counts <- FeatureMatrix(
+  fragments = Fragments(pbmc),
+  features = peaks,
+  cells = colnames(pbmc)
+)
+
+# create a new assay using the MACS2 peak set and add it to the Seurat object
+pbmc[["peaks"]] <- CreateChromatinAssay(
+  counts = macs2_counts,
+  fragments = fragpath,
+  annotation = annotation
+)
+DefaultAssay(pbmc) <- "RNA"
+pbmc <- SCTransform(pbmc)
+pbmc <- RunPCA(pbmc)
+
+DefaultAssay(pbmc) <- "peaks"
+pbmc <- FindTopFeatures(pbmc, min.cutoff = 5)
+pbmc <- RunTFIDF(pbmc)
+pbmc <- RunSVD(pbmc)
+
+#
+# build a joint neighbor graph using both assays
+DefaultAssay(pbmc) <- "SCT"
+pbmc <- FindMultiModalNeighbors(
+  object = pbmc,
+  reduction.list = list("pca", "lsi"), 
+  dims.list = list(1:50, 2:40),
+  modality.weight.name = "RNA.weight",
+  verbose = TRUE
+)
+
+# build a joint UMAP visualization
+pbmc <- RunUMAP(
+  object = pbmc,
+  nn.name = "weighted.nn",
+  assay = "RNA",
+  verbose = TRUE
+)
+DefaultAssay(pbmc) <- "peaks"
+
+# first compute the GC content for each peak
+pbmc <- RegionStats(pbmc, genome = BSgenome.Hsapiens.UCSC.hg38)
+
+# link peaks to genes
+pbmc <- LinkPeaks(
+  object = pbmc,
+  peak.assay = "peaks",
+  expression.assay = "SCT"
+)
+
+# output
+saveRDS(pbmc, "pbmc.rds")
